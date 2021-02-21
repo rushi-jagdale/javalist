12134758bd138ebe921805c3ea4dbb27ba73d337
rushi.jagdale <rjcom99@gmail.com>
2021-02-21 22:00:05 +0530

    adding java list file

---
+++ linkedlist.java
@@ -0,0 +1,40 @@
+public class linkedlist{
+	Node head;
+   public class Node{
+	   int data;
+	   Node next;
+	   Node(int d)
+	   {
+		   data=d;
+		   next=null;
+	   }
+   }
+   public void  insert(int data)
+   {
+       
+       Node n=new Node();
+       n=data;
+       if(head==null)
+       {
+	       head=n;
+       }
+       else
+       {
+	       Node n;
+	       while(head!=null)
+	       {
+		       head=head.next;
+	       }
+	       head=data;
+		       
+
+   }
+   }
+
+  public static void main(String[] arg)
+   {
+	   linkedlist list=new linkedlist();
+	   list.insert();
+   }
+}
+
